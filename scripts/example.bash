b4a7c90a66d6cdf48b9419f5eddc382399250e18
Adam Berlin <aberlin@pivotal.io>
2019-01-26 21:39:27 -0500

    Extract repository into environment.

--- scripts/example.bash
+++ scripts/example.bash
@@ -2,6 +2,7 @@
 
 export PIVOTAL_TRACKER_PROJECT_ID='2230629'
 export PIVOTAL_TRACKER_LABEL='example-issue'
+export GITHUB_REPO='berlin-ab/gpdb'
 export PYTHONPATH=$PWD:$PYTHONPATH
 
 ./bin/github_tracker_cli
