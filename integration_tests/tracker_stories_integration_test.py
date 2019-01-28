499a65f70e51df0dc7f03bca96b92138ed4c0f02
Adam Berlin <aberlin@pivotal.io>
2019-01-27 19:59:14 -0500

    Add test to ensure icebox stories are searched.

--- integration_tests/tracker_stories_integration_test.py
+++ integration_tests/tracker_stories_integration_test.py
@@ -24,11 +24,21 @@ class TrackerStoriesIntegrationTest(unittest.TestCase):
 
         self.assertIn(162506314, [story.story_id() for story in stories])
         self.assertIn('English auction', [story.title() for story in stories])
-
+        
     def test_api_throws_missing_api_token_error(self):
         with self.assertRaises(MissingPivotalTrackerApiTokenError):
             tracker_api = PivotalTrackerApi(api_token=None)
                         
+    def test_it_returns_stories_from_the_icebox(self):
+        tracker_api = PivotalTrackerApi(api_token=get_api_token())
+        
+        stories = TrackerStories(tracker_api).fetch_by_label(
+            project_id=2230629,
+            label='example-label'
+            )
+
+        self.assertIn('Example icebox story', [story.title() for story in stories])
+
 
 
 
