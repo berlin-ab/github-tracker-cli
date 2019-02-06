5da23a895509dbe5dffe1c8688e69087675162cf
Adam Berlin <aberlin@pivotal.io>
2019-02-06 16:27:03 -0500

    Improve build output.

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,24 +3,32 @@
 
 set -e
 
+log() {
+    echo
+    echo $1
+    echo
+}
 
+
+log "clean before running suite"
 LOG_FILE=/tmp/github-tracker-cli-build.log
 rm -f $LOG_FILE # cleanup previous run
 
 
-# install dependencies
+log "install dependencies"
 pip install -r requirements.txt
 pip install -r requirements-development.txt
 
 
-# run test suites
+log "run test suites"
 ./scripts/unit-test.sh
 ./scripts/code-quality-check.bash
 ./scripts/integration-test.sh
 
 
-# run examples
+log "run examples"
 ./scripts/example-missing-stories.bash >> $LOG_FILE && echo "Passed." || (echo 'Failed.' && false)
 ./scripts/example-missing-stories-csv.bash >> $LOG_FILE && echo "Passed." || (echo 'Failed.' && false)
 ./scripts/example-closed-issues.bash >> $LOG_FILE && echo "Passed." || (echo 'Failed' && false)
 ./scripts/example-pull-requests.bash >> $LOG_FILE && echo "Passed." || (echo 'Failed' && false)
+
