d321178724ac7594a5ae7bbc32241c09bf09d410
Adam Berlin <aberlin@pivotal.io>
2019-02-06 10:46:32 -0500

    Improve the build script to have better output.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,26 @@
-#!/usr/bin/env bash
+ #!/usr/bin/env bash
+
 
 set -e
 
+
+LOG_FILE=/tmp/github-tracker-cli-build.log
+rm -f $LOG_FILE # cleanup previous run
+
+
+# install dependencies
 pip install -r requirements.txt
 pip install -r requirements-development.txt
 
+
+# run test suites
 ./scripts/unit-test.sh
 ./scripts/code-quality-check.bash
-
 ./scripts/integration-test.sh
 
-./scripts/example-missing-stories.bash
-./scripts/example-missing-stories-csv.bash
-./scripts/example-closed-issues.bash
+
+# run examples
+./scripts/example-missing-stories.bash >> $LOG_FILE && echo "Passed." || (echo 'Failed.' && false)
+./scripts/example-missing-stories-csv.bash >> $LOG_FILE && echo "Passed." || (echo 'Failed.' && false)
+./scripts/example-closed-issues.bash >> $LOG_FILE && echo "Passed." || (echo 'Failed' && false)
+./scripts/example-pull-requests.bash >> $LOG_FILE && echo "Passed." || (echo 'Failed' && false)
