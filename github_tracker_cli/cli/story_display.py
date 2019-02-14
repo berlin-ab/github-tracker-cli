a41040922f186ce08b1a9d2e9fb6c5da29031ecf
Adam Berlin <aberlin@pivotal.io>
2019-02-14 03:43:47 +0000

    Unicode function only available in 2.7

--- github_tracker_cli/cli/story_display.py
+++ github_tracker_cli/cli/story_display.py
@@ -11,16 +11,14 @@ def display_stories_as_rows(stories, printer):
 
 
 def display_history_as_rows(story_histories, printer):
-    printer("story id   | duration since started | title")
-
     for story_history in story_histories:
         started_duration_in_days = "%d days" % story_history.started_duration_in_days()
             
         printer(
             u"{story_id} | {started_duration} | {title}".format(
-                story_id=unicode(story_history.story().story_id()).ljust(10),
+                story_id=(u'%d' % story_history.story().story_id()).ljust(10),
                 title=story_history.story().title(),
-                started_duration=unicode(started_duration_in_days).ljust(22)
+                started_duration=started_duration_in_days.ljust(8)
             )
         )
     
