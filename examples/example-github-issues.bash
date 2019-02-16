44565e3dbce2bb0a4f088ddf2ceac44d7319e0c7
Adam Berlin <aberlin@pivotal.io>
2019-02-16 18:37:48 +0000

    Cleanup build script, add example for github-issues.

---
+++ examples/example-github-issues.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+./bin/github_tracker_cli github-issues \
+			 --github-repo="berlin-ab/github-tracker-cli" \
+			 "$@";
+
--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,9 @@
 set -e
 
 
-python --version
+print_python_version() {
+    python --version
+}
 
 
 log() {
@@ -14,32 +16,85 @@ log() {
 }
 
 
-export PYTHONIOENCODING=utf_8
+run_example() {
+    local example_script=$1;
+
+    $example_script >> $LOG_FILE && \
+	(echo "Passed $example_script") || \
+        (echo "Failed $example_script" && false);
+}
 
 
-log "clean before running suite"
-LOG_FILE=/tmp/github-tracker-cli-build.log
-rm -f $LOG_FILE # cleanup previous run
-find . -name *.pyc | xargs --no-run-if-empty rm;
-find . -name __pycache__ | xargs --no-run-if-empty rm -rf;
+setup_log_file() {
+    log "clean before running suite"
+    LOG_FILE=/tmp/github-tracker-cli-build.log
+    rm -f $LOG_FILE # cleanup previous run
+    find . -name *.pyc | xargs --no-run-if-empty rm;
+    find . -name __pycache__ | xargs --no-run-if-empty rm -rf;
+}
 
 
-
-log "install dependencies"
-pip install -r requirements.txt >> $LOG_FILE && echo "Success." || (echo "Failed to install dependencies" && false)
-pip install -r requirements-development.txt >> $LOG_FILE && echo "Success." || (echo "Failed to install dev dependencies" && false)
-
-
-log "run test suites"
-./scripts/unit-test.sh
-./scripts/code-quality-check.bash
-./scripts/integration-test.sh
+install_production_dependencies() {
+    pip install -r requirements.txt >> $LOG_FILE && \
+	(echo "Success.") || \
+        (echo "Failed to install dependencies" && false);
+}
 
 
-log "run examples"
-./examples/example-missing-stories.bash >> $LOG_FILE && echo "Passed." || (echo 'Failed.' && false)
-./examples/example-missing-stories-csv.bash >> $LOG_FILE && echo "Passed." || (echo 'Failed.' && false)
-./examples/example-closed-issues.bash >> $LOG_FILE && echo "Passed." || (echo 'Failed' && false)
-./examples/example-pull-requests.bash >> $LOG_FILE && echo "Passed." || (echo 'Failed' && false)
-./examples/example-tracker-story-history.bash >> $LOG_FILE && echo "Passed." || (echo 'Failed' && false)
+install_development_dependencies() {
+    pip install -r requirements-development.txt >> $LOG_FILE && \
+	(echo "Success.") || \
+	(echo "Failed to install dev dependencies" && false);    
+}
 
+
+install_dependencies() {
+    install_production_dependencies
+    install_development_dependencies
+}
+
+
+run_unit_tests() {
+    ./scripts/unit-test.sh
+}
+
+
+run_code_quality_check() {
+    ./scripts/code-quality-check.bash
+}
+
+
+run_integration_tests() {
+    ./scripts/integration-test.sh
+}
+
+
+run_examples() {
+    run_example "./examples/example-missing-stories.bash"
+    run_example "./examples/example-missing-stories-csv.bash"
+    run_example "./examples/example-closed-issues.bash"
+    run_example "./examples/example-pull-requests.bash"
+    run_example "./examples/example-tracker-story-history.bash"
+    run_example "./examples/example-github-issues.bash"
+}
+
+
+main() {
+    export PYTHONIOENCODING=utf_8
+    setup_log_file
+    print_python_version
+
+    log "install dependencies"
+    install_dependencies
+
+    log "run code quality check"
+    run_code_quality_check
+    
+    log "run test suites"
+    run_unit_tests
+    run_integration_tests
+    run_examples
+}
+
+
+main "$@"
